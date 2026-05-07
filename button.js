90dc313cd829ee0dcf75b63e45f3a6ada141f5df
Pilirreos64 <ericdaniel.barreto@gmail.com>
2026-05-07 12:46:34 -0500

    sisongas

--- button.js
+++ button.js
@@ -6,3 +6,40 @@ let panelCarrito = document.getElementById('side-cart');
 let contenedorItems = document.getElementById('cart-items');
 let textoTotal = document.getElementById('cart-total');
 let contadorIcono = document.getElementById('cart-count');
+
+botonAbrir.onclick = function() {
+    panelCarrito.classList.add('active');
+}
+
+botonCerrar.onclick = function() {
+    panelCarrito.classList.remove('active');
+}
+
+let botonesAñadir = document.querySelectorAll('.btn-add');
+
+botonesAñadir.forEach(function(boton) {
+    boton.onclick = function(evento) {
+        let tarjeta = evento.target.closest('.game-card');
+        
+        
+        let nombre = tarjeta.querySelector('h3').innerText;
+        let precio = parseFloat(tarjeta.getAttribute('data-price'));
+        let imagenUrl = tarjeta.querySelector('img').src;
+        
+      
+        let desc = tarjeta.querySelector('.description').innerText;
+        let calif = tarjeta.querySelector('.rating span').innerText;
+
+      
+        let juegoNuevo = {
+            titulo: nombre,
+            costo: precio,
+            imagen: imagenUrl,
+            descripcion: desc,      
+            calificacion: calif     
+        };
+
+        listaCarrito.push(juegoNuevo);
+        actualizarPantalla();
+    }
+});
