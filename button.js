af500095d0c505bd2aeab3066ba88cdd22ac3bf8
Pilirreos64 <ericdaniel.barreto@gmail.com>
2026-05-07 12:39:53 -0500

    hacele

---
+++ button.js
@@ -0,0 +1,8 @@
+let listaCarrito = [];
+
+let botonAbrir = document.getElementById('cart-icon');
+let botonCerrar = document.getElementById('close-cart');
+let panelCarrito = document.getElementById('side-cart');
+let contenedorItems = document.getElementById('cart-items');
+let textoTotal = document.getElementById('cart-total');
+let contadorIcono = document.getElementById('cart-count');
